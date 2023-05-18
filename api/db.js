19919464cc7a670dccd9907c1998e96c587123c7
Melvinoh <murichumelvin@gmail.com>
2023-05-19 02:40:15 +0300

    api db connection

--- api/db.js
+++ api/db.js
@@ -1,1 +1,17 @@
 
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false )
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('db connected')
+    } catch (error) {
+        console.log(' db not connected')
+    }
+}
+
+export default db
