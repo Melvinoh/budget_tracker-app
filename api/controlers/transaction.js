d9487c2bd288a10556ac4f8d46863048b7108e9d
Melvinoh <murichumelvin@gmail.com>
2023-07-03 12:12:45 +0300

    uml update

--- api/controlers/transaction.js
+++
@@ -1,3 +0,0 @@
-export const transaction = (req , res)=>{
-
-}
--- api/routes/transactionRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {transaction} from '../controlers/transaction.js'
-
-const router = express.Router()
-
-router.post('/transactions' , transaction );
-
-export default router;
